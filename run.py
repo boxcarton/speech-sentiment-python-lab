2434888a079be82164e0d4899d04f18b505d5c5c
Josh Zheng <denial@gmail.com>
2016-02-22 13:14:35 -0800

    Added .env, removed code in main

--- run.py
+++ run.py
@@ -8,24 +8,11 @@ from watson_developer_cloud import AlchemyLanguageV1 as AlchemyLanguage
 from speech_sentiment_python.recorder import Recorder
 
 def transcribe_audio(path_to_audio_file):
-    username = os.environ.get("BLUEMIX_USERNAME")
-    password = os.environ.get("BLUEMIX_PASSWORD")
-    speech_to_text = SpeechToText(username=username,
-                                  password=password)
-
-    with open(join(dirname(__file__), path_to_audio_file), 'rb') as audio_file:
-        return speech_to_text.recognize(audio_file,
-            content_type='audio/wav')
+    pass
 
 def get_text_sentiment(text):
-    alchemy_api_key = os.environ.get("ALCHEMY_API_KEY")
+    pass
     
-    alchemy_language = AlchemyLanguage(api_key=alchemy_api_key)
-    result = alchemy_language.sentiment(text=text)
-    if result['docSentiment']['type'] == 'neutral':
-        return 'netural', 0
-    return result['docSentiment']['type'], result['docSentiment']['score']
-
 def main():
     recorder = Recorder("speech.wav")
 
